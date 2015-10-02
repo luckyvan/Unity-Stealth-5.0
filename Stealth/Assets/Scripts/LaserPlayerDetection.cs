f4e0856feba21abc61c7b9549f9c32b63dbd1e44
Fan Chou <Chou.Fan@igt.com>
2015-10-02 15:42:10 +0800

    FIX: let laser fense can detect  player.

--- Stealth/Assets/Scripts/LaserPlayerDetection.cs
+++ Stealth/Assets/Scripts/LaserPlayerDetection.cs
@@ -10,16 +10,16 @@ public class LaserPlayerDetection : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag (Tags.player);
-		
 		lastPlayerSighting = GameObject.FindWithTag (Tags.gameController).GetComponent<LastPlayerSighting>();
 	}
 	
 	void OnTriggerStay (Collider other) {
 		if (GetComponent<Renderer>().enabled) {
 			if (other.gameObject == player) {
-				//determin whether player is in the light view
-				Vector3 relPlayerPosition = player.transform.position - transform.position;
-			}	
+				// ... set the last global sighting of the player to the colliding object's position.
+				lastPlayerSighting.position = other.transform.position;
+
+			}
 		}
 
 	}
