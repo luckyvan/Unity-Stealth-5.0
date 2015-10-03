b4c9643279c90d7665f10a04bc44868d74fb2e5e
Fan Chou <Chou.Fan@igt.com>
2015-10-03 11:15:35 +0800

    4.3.1 add switch 001 - 004

---
+++ Stealth/Assets/Scripts/LaserSwitchDeactivation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSwitchDeactivation : MonoBehaviour {
+
+	public GameObject laser; //laser fense to be controlled by this Switch
+
+	public Material unlockedMaterial;
+
+	public GameObject player;
+
+	private GameObject screen; //the screen to indicate whether the switch is locked or unlocked.
+
+	private AudioSource audioSource;
+
+	void Awake (){
+	
+		audioSource = GetComponent<AudioSource>();
+
+		screen = transform.Find("prop_switchUnit_screen").gameObject;
+
+	}
+
+	void LaserDeactivation (){
+		laser.SetActive(false);
+
+		screen.GetComponent<Renderer>().material = unlockedMaterial;
+
+		audioSource.Play ();
+	}
+	
+	void OnTriggerStay (Collider other){
+		if (other.gameObject == player) {
+			if (Input.GetButton ("Switch")) {
+				LaserDeactivation ();
+			}
+		}
+
+	}
+}
