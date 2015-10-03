0a9c479ede9ab9c7bf0ce28fb3cbfb647e8d95c0
Fan Chou <Chou.Fan@igt.com>
2015-10-03 14:43:52 +0800

    4.3.2 key and key grab.

---
+++ Stealth/Assets/Scripts/KeyPickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPickup : MonoBehaviour {
+
+	public AudioClip keyGrab; // audio played when pick up the key
+
+	private GameObject player;
+
+	private PlayerInventory playerInventory;
+
+	void Awake () {
+		player = GameObject.FindWithTag (Tags.player);
+		playerInventory = player.GetComponent<PlayerInventory>();
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject == player) {
+			AudioSource.PlayClipAtPoint (keyGrab, transform.position);
+
+			playerInventory.hasKey = true;
+
+			Destroy (gameObject);
+		}
+	}
+}
