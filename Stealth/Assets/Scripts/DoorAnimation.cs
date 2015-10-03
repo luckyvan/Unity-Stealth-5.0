a16ffcda37d55c733fb18672b7d8ddd695413bd7
Fan Chou <Chou.Fan@igt.com>
2015-10-03 15:32:16 +0800

    4.3.3 add slide door. don't understand why need to play the audio loop here.

---
+++ Stealth/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimation : MonoBehaviour {
+
+	public bool requireKey;
+
+	public AudioClip doorSwitchClip;   // open or close door audio
+
+	public AudioClip accessDeniedClip;
+
+	private Animator anim;
+
+	private GameObject player;
+
+	private HashIDs hash;
+
+	private PlayerInventory playerInventory;
+
+	private AudioSource audioSource;
+
+	private int count;
+
+	void Awake () {
+		anim = GetComponent<Animator>();
+
+		hash = GameObject.FindWithTag (Tags.gameController).GetComponent<HashIDs>();
+
+		player = GameObject.FindWithTag (Tags.player);
+
+		playerInventory = player.GetComponent<PlayerInventory>();
+
+		audioSource = GetComponent<AudioSource>();
+
+		count = 0;
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject == player) {
+			if (requireKey) {
+				if (playerInventory.hasKey) {
+					count++;
+				}else{
+					audioSource.clip = accessDeniedClip;
+					audioSource.Play ();
+				}
+			}else{
+				count++;
+			}
+		}
+	}
+
+	void OnTriggerExit (Collider other){
+		if (other.gameObject == player) {
+			count = Mathf.Max (0, count - 1);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool(hash.openBool, count > 0);
+		if (anim.IsInTransition (0) && !audioSource.isPlaying) {
+			audioSource.clip = doorSwitchClip;
+			audioSource.Play ();
+		}
+	}
+}
